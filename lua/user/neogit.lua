5cf9c0136add86f6cfedbad412a34b249ac8830b
devel-elel <devellelle@gmail.com>
2022-07-17 18:29:13 +0200

    fixed bad mapping default

--- lua/user/neogit.lua
+++ lua/user/neogit.lua
@@ -71,9 +71,9 @@ neogit.setup {
     -- modify status buffer mappings
     status = {
       -- Adds a mapping with "B" as key that does the "BranchPopup" command
-      ["B"] = "BranchPopup",
+      --["B"] = "BranchPopup",
       -- Removes the default mapping of "s"
-      ["s"] = "",
+      --["s"] = "",
     }
   }
 }
