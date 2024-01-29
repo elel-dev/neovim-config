4057c9b51bceb3dbd2ff4ea6ee82d3825b6c2476
lorenzolarghi <elel.develop@gmail.com>
2024-01-29 17:38:05 +0100

    updates

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -1,117 +1,23 @@
--- following options are mostyle the default ones
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  print("nvim-tree package is missing")
-  return
-end
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  print("nvim-tree.config is missing or nill")
-  return
-end
+-- set termguicolors to enable highlight groups
+-- vim.opt.termguicolors = true
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-local function on_attach(bufnr)
-  local api = require('nvim-tree.api')
-
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- Default mappings. Feel free to modify or remove as you wish.
-  --
-  -- BEGIN_DEFAULT_ON_ATTACH
-  vim.keymap.set('n', 'a',     api.fs.create,                         opts('Create'))
-  vim.keymap.set('n', 'd',     api.fs.remove,                         opts('Delete'))
-  vim.keymap.set('n', 'r',     api.fs.rename,                         opts('Rename'))
-  vim.keymap.set('n', 'x',     api.fs.cut,                            opts('Cut'))
-  -- END_DEFAULT_ON_ATTACH
-
-  -- Mappings migrated from view.mappings.list
-  --
-  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
-  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
-
-end
-
-nvim_tree.setup {
-  on_attach = on_attach,
-  disable_netrw = true,
-  hijack_netrw = true,
-  --[[ open_on_setup = false, ]]
-  --[[ ignore_ft_on_setup = { ]]
-  --[[   "startify", ]]
-  --[[   "dashboard", ]]
-  --[[   "alpha", ]]
-  --[[ }, ]]
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
+-- setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
   },
   view = {
-    width = 35,
-    -- height = 30,
-    hide_root_folder = false,
-    side = "left",
-    --[[ mappings = { ]] -- DEPRECATED, SEE ON_ATTACH
-    --[[   custom_only = false, ]]
-    --[[   list = { ]]
-    --[[     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" }, ]]
-    --[[     { key = "h", cb = tree_cb "close_node" }, ]]
-    --[[     { key = "s", cb = tree_cb "vsplit" }, ]]
-    --[[   }, ]]
-    --[[ }, ]]
-    number = false,
-    relativenumber = false,
+    width = 30,
   },
   renderer = {
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      },
-    }
-  }
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 }
-
+)
